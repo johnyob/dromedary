b89aa0efa64fa0f2e963943d941bdb2a92e73dea
johnyob <ajo41@cam.ac.uk>
2021-10-21 16:06:06 +0100

    Implement a path compression + rank-based disjoint set data structure.

---
+++ src/typing/constraints/union_find.mli
@@ -0,0 +1,50 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** This module implements an efficient union-find algorithm
+    on disjoint sets, based on Tarjan and Huet. See IA Algorithms notes. *)
+
+(** The type ['a t] denotes a node within a given disjoint set. 
+    ['a] is the type of the value (descriptor) of the node. *)
+
+type 'a t [@@deriving sexp_of]
+
+(* ------------------------------------------------------------------------- *)
+
+(** [fresh desc] creates a fresh node. It forms it's disjoint set, with 
+    descriptor [desc]. *)
+val fresh : 'a -> 'a t
+
+(** [find node] returns the descriptor associated w/ [node]'s set. *)
+val find : 'a t -> 'a
+
+(** [set node desc] updates the descriptor of [node] to [desc]. *)
+val set : 'a t -> 'a -> unit
+
+(** [union node1 node2 ~f] merges the disjoint sets associated with [node1]
+    and [node2]. The descriptors are merged using the function [~f]. *)
+val union : 'a t -> 'a t -> f:('a -> 'a -> 'a) -> unit
+
+(* ------------------------------------------------------------------------- *)
+
+(** [node1 === node2] returns true if [node1] and [node2] belong to the same
+    disjoint set. *)
+val ( === ) : 'a t -> 'a t -> bool
+
+(* TODO: Is it worth exposing these? They're based on implementation details *)
+
+(** [is_root node] returns [true] if [node] is a "root" node of the disjoint 
+    set. *)
+val is_root : 'a t -> bool
+
+val is_child : 'a t -> bool
