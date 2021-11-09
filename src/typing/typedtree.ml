e0ac0c7e523be6af7ed3868dbf71616cc1ef5f30
johnyob <ajo41@cam.ac.uk>
2021-11-09 16:27:25 +0000

    Define the types and typedtree for procaml

---
+++ src/typing/typedtree.ml
@@ -0,0 +1,94 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Ast_types
+open Types
+
+(* ------------------------------------------------------------------------- *)
+
+(** Abstract syntax tree after typing *)
+
+type pattern = 
+  { pat_desc : pattern_desc
+  ; pat_type : type_expr
+  }
+
+and pattern_desc =
+  | Tpat_any
+  (** [_] *)
+  | Tpat_var of string 
+  (** [x]. *)
+  | Tpat_alias of pattern * string 
+  (** [P as x]. *)
+  | Tpat_constant of constant 
+  (** [c]. e.g. [1, true, ()]. *)
+  | Tpat_tuple of pattern list 
+  (** (P1, ..., Pn). Invariant n >= 2. *)
+  | Tpat_construct of constructor_description * pattern option 
+  (** [C <P>]. *)
+  | Tpat_record of (label_description * pattern) list
+  (** { l1=P1; ...; ln=Pn } *)
+
+type expression = 
+  { exp_desc : expression_desc
+  ; exp_type : type_expr
+  } 
+  
+and expression_desc =
+  | Texp_var of string 
+  (** [x]. *)
+  | Texp_prim of primitive
+  (** Primitive operations [%prim p]. e.g. [%prim +], [%prim -], etc. *)
+  | Texp_const of constant 
+  (** Constants [c]. e.g. [1, true, ()]. *)
+  | Texp_constraint of expression * type_expr
+  (** An expression "constraint" or typing annotation [(E : T)]. *)
+  | Texp_fun of pattern list * expression
+  (** The function (or lambda) abstraction [fun P -> E].  
+      Note that: 
+        - [let x P1 ... Pn = E in ...] is encoding using 
+          [Pexp_let ("x", fun P1 ... Pn -> E, ...)]. *)
+  | Texp_app of expression * expression 
+  (** Function application [E1 E2]. *)
+  | Pexp_let of rec_flag * value_binding * expression
+  (** Let expressions *)
+  | Texp_forall of string list * expression
+  (** Explicit "forall" quantifier [forall 'a1 ... 'an -> E]. 
+      Invariant n >= 1. *)
+  | Texp_tapp of expression * type_expr
+  (** Explicit type application *)
+  | Texp_construct of constructor_description * expression option
+      (** An applied algebraic data type constructor [C <E>]. *)
+  | Texp_record of (label_description * expression) list 
+  (** {l1 = E1; ...; ln = En} *)
+  | Texp_field of expression * label_description 
+  (** E.l *)
+  | Texp_tuple of expression list
+  (** Tuples [(E1, ..., En)]. 
+      Invariant: n >= 2. *)
+  | Texp_match of expression * case list
+  (** Match (or "case") expressions [match E with (P1 -> E1 | ... | Pn -> En)]. *)
+  | Texp_ifthenelse of expression * expression * expression
+  (** If (or ternary) expressions [if E then E1 else E2]. *)
+
+(** [P = E]. *)
+and value_binding =
+  { tvb_pat : pattern
+  ; tvb_expr : expression
+  }
+
+(** [P -> E]. *)
+and case =
+  { tc_lhs : pattern
+  ; tc_rhs : expression
+  }
---
+++ src/typing/types.ml
@@ -0,0 +1,72 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Ast_types
+
+(* ------------------------------------------------------------------------- *)
+
+(** Representation of types and declarations  *)
+
+type type_expr =
+  | Ttyp_var of string
+  (** Type variables ['a]. *)
+  | Ttyp_arrow of type_expr * type_expr
+  (** Function types [T1 -> T2]. *)
+  | Ttyp_tuple of type_expr list
+  (** Product (or "tuple") types. *)
+  | Ttyp_constr of type_constr
+  (** Type constructors. *)
+  | Ttyp_forall of string * type_expr
+  (** Forall type. *)
+
+and type_constr = type_expr list * string
+
+(* ------------------------------------------------------------------------- *)
+
+(* Type definitions *)
+
+type type_declaration =
+  { type_name : string
+  ; type_params : string list
+  ; type_kind : type_decl_kind
+  }
+
+and type_decl_kind =
+  | Type_record of label_declaration list
+  | Type_variant of constructor_declaration list
+
+and label_declaration =
+  { label_name : string
+  ; label_type : type_expr
+  }
+
+and constructor_declaration =
+  { constructor_name : string
+  ; constructor_arg : type_expr option
+  }
+
+(* ------------------------------------------------------------------------- *)
+
+(* Constructor and record label descriptions *)
+
+type constructor_description = 
+  { constructor_name : string
+  ; constructor_arg  : type_expr option
+  ; constructor_type : type_expr 
+  }
+
+type label_description = 
+  { label_name : string
+  ; label_arg  : type_expr
+  ; label_type : type_expr
+  }
