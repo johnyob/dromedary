ede6b5e9aa3db676114da4985112ac1f767b8e0f
johnyob <ajo41@cam.ac.uk>
2021-11-22 19:58:59 +0000

    Define public exports for constraints library.

---
+++ src/typing/constraints/constraints.mli
@@ -0,0 +1,32 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open! Import
+
+module Make (Algebra : Algebra) : sig
+
+  module Constraint : module type of Constraint.Make (Algebra)
+
+  module Computation : sig
+    module type Basic = Computation.Basic
+    module type Basic2 = Computation.Basic2
+
+    module type S = Computation.S
+    module type S2 = Computation.S2
+
+    module Make (Basic : Basic) : module type of Computation.Make (Algebra) (Basic)
+
+    module Make2 (Basic : Basic2) : module type of Computation.Make2 (Algebra) (Basic)
+  end
+
+end
