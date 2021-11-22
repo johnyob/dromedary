31a60a8e112a85b8b3a108c3727749ad26c56fa5
johnyob <ajo41@cam.ac.uk>
2021-11-22 19:58:23 +0000

    Implement Computation.

---
+++ src/typing/constraints/import.ml
@@ -0,0 +1,21 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+include Base
+
+module Private = struct
+  module Constraint = Dromedary_typing_constraints_constraint.Constraint
+  module Solver = Dromedary_typing_constraints_solver.Solver
+end
+
+include Private.Constraint.Module_types
