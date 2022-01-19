7d5eaa8dfbd5261ff4e54390bd6e4be52fc60ba2
johnyob <ajo41@cam.ac.uk>
2022-01-19 22:06:21 +0000

    Introduce the `structure` abstraction into the unifier (WIP)

---
+++ src/constraints/solver/structure.ml
@@ -0,0 +1,281 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open! Import
+include Structure_intf
+
+module Rigid_var = struct
+  module T = struct
+    type t = int [@@deriving sexp_of, compare]
+  end
+
+  include T
+  include Comparator.Make (T)
+
+  let make =
+    let next = ref 0 in
+    fun () -> post_incr next
+
+
+  let hash t = t
+end
+
+module Ambivalence (Structure : S) = struct
+  module Rigid_type = struct
+    type t =
+      | Rigid_var of Rigid_var.t
+      | Structure of t Structure.t
+  end
+
+  module Equations = struct
+    module Scope = struct
+      type t = int
+
+      let min = min
+      let max = max
+      let outermost_scope = 0
+    end
+
+    module Ctx = struct
+      type t =
+        ( Rigid_var.t
+        , Scope.t * Rigid_type.t
+        , Rigid_var.comparator_witness )
+        Map.t
+
+      let empty = Map.empty (module Rigid_var)
+
+      exception Inconsistent
+
+      let add _t _rigid_type1 _rigid_type2 _scope = assert false
+      let get_equation t rigid_var = Map.find t rigid_var
+    end
+  end
+
+  type 'a t =
+    { mutable structure : 'a Structure.t scoped option
+    ; scope : Equations.Scope.t
+    ; rigid_vars : (Rigid_var.t, bool scoped) Hashtbl.t
+    }
+
+  and 'a scoped = 'a * Equations.Scope.t
+
+  let make_rigid_var rigid_var =
+    let rigid_vars = Hashtbl.create (module Rigid_var) in
+    Hashtbl.set
+      rigid_vars
+      ~key:rigid_var
+      ~data:(true, Equations.Scope.outermost_scope);
+    { structure = None; scope = Equations.Scope.outermost_scope; rigid_vars }
+
+
+  let make_structure structure =
+    { structure =
+        Option.(
+          structure
+          >>| fun structure -> structure, Equations.Scope.outermost_scope)
+    ; scope = Equations.Scope.outermost_scope
+    ; rigid_vars = Hashtbl.create (module Rigid_var)
+    }
+
+
+  let structure t = t.structure
+  let scope t = t.scope
+  let rigid_vars t = Hash_set.of_hashtbl_keys t.rigid_vars
+
+  let iter t ~f =
+    Option.iter t.structure ~f:(fun (structure, _) ->
+        Structure.iter structure ~f)
+
+
+  let fold t ~f ~init =
+    Option.fold t.structure ~init ~f:(fun init (structure, _) ->
+        Structure.fold structure ~init ~f)
+
+
+  let map t ~f =
+    { t with
+      structure =
+        Option.map t.structure ~f:(fun (structure, scope) ->
+            Structure.map structure ~f, scope)
+    }
+
+
+  type ctx = Equations.Ctx.t * Structure.ctx
+
+  let ectx = fst
+  let sctx = snd
+
+  type term =
+    { fold : 'a. 'a t -> 'a
+    ; unfold : 'a. 'a -> 'a t
+    }
+
+  exception Cannot_merge
+
+  let merge_scoped scoped1 scoped2 ~f =
+    let t1, scope1 = scoped1 in
+    let t2, scope2 = scoped2 in
+    f t1 t2, Equations.Scope.min scope1 scope2
+
+
+  let decompose_scope (_, scope1) (_, scope2) =
+    Equations.Scope.max scope1 scope2
+
+
+  let merge_structure t1 t2 =
+    match t1.structure, t2.structure with
+    | None, structure | structure, None -> structure
+    | Some structure1, Some structure2 ->
+      (* We select an arbitrary structure, namely the first.*)
+      Some
+        (merge_scoped structure1 structure2 ~f:(fun structure _ -> structure))
+
+
+  let merge_rigid_vars t1 t2 =
+    Hashtbl.merge t1.rigid_vars t2.rigid_vars ~f:(fun ~key:_ -> function
+      | `Left desc | `Right desc -> Some desc
+      | `Both (desc1, desc2) ->
+        Some
+          (merge_scoped desc1 desc2 ~f:(fun is_expansive1 is_expansive2 ->
+               is_expansive1 && is_expansive2)))
+
+
+  let find_decomposable_rigid_var_scope t1 t2 =
+    Hash_set.inter (rigid_vars t1) (rigid_vars t2)
+    |> Hash_set.fold ~init:None ~f:(fun acc rigid_var ->
+           let find_scope t = Hashtbl.find_exn t.rigid_vars rigid_var |> snd in
+           let scope' = Equations.Scope.min (find_scope t1) (find_scope t2) in
+           match acc with
+           | None -> Some scope'
+           | Some scope -> if scope' < scope then Some scope' else Some scope)
+
+
+  let get_min_expansive ~ctx t =
+    (* TODO: Come up w/ clever datastructure to compute this efficiently *)
+    Hashtbl.fold
+      t.rigid_vars
+      ~init:None
+      ~f:(fun ~key:rigid_var ~data:(is_expansive, rigid_var_scope) acc ->
+        if not is_expansive
+        then acc
+        else (
+          match acc, Equations.Ctx.get_equation (ectx ctx) rigid_var with
+          | None, Some rigid_type ->
+            Some ((rigid_var, rigid_var_scope), rigid_type)
+          | Some (_, rigid_type), Some rigid_type'
+            when snd rigid_type' < snd rigid_type ->
+            Some ((rigid_var, rigid_var_scope), rigid_type')
+          | _ -> acc))
+
+
+  exception Cannot_decompose
+
+  let decompose ~term ~ctx ~equate t1 t2 =
+    (* In order to decompose 2 ambivalent types, we determine the common member 
+       (if one exists) with the smallest "scope" to decompose "on". *)
+    (* There are 2 rules for decomposition of ambivalent types:
+       - decomposing on the structure
+       - decomposing via a common rigid variable *)
+    match find_decomposable_rigid_var_scope t1 t2 with
+    | Some scope ->
+      (* We can decompose on the common rigid var [rigid_var] between [t1] 
+         and [t2]. *)
+      (* From [find_decomposable_rigid_var], we know that [scope] the minimum
+         scope for a common rigid variable. *)
+      let scope =
+        Equations.Scope.max t1.scope t2.scope |> Equations.Scope.max scope
+      in
+      let structure = merge_structure t1 t2 in
+      let rigid_vars = merge_rigid_vars t1 t2 in
+      { structure; rigid_vars; scope }
+    | None ->
+      (* If there is no-common rigid variable, then we attempt to decompose on the 
+         structure *)
+      (match Option.both t1.structure t2.structure with
+      | None -> raise Cannot_decompose
+      | Some (structure1, structure2) ->
+        (* Determine the merged structure *)
+        let structure =
+          (* let term = 
+            { fold = fun structure -> make_structure (Some structure) |> term.fold 
+            ; unfold = fun t -> term.unfold t |> structure   }
+          in *)
+          merge_scoped
+            structure1
+            structure2
+            ~f:(Structure.merge ~term ~ctx:(sctx ctx) ~equate)
+        in
+        (* Compute the required scope for the decomposition. *)
+        let scope =
+          Equations.Scope.max t1.scope t2.scope
+          |> Equations.Scope.max (decompose_scope structure1 structure2)
+        in
+        (* Merge the rigid variables *)
+        let rigid_vars = merge_rigid_vars t1 t2 in
+        { structure = Some structure; scope; rigid_vars })
+
+
+  exception Cannot_expand
+
+  let convert_rigid_type ~term rigid_type =
+    term.fold
+      (match rigid_type with
+      | Rigid_type.Rigid_var rigid_var -> make_rigid_var rigid_var
+      | Rigid_type.Structure structure -> make_structure (Some structure))
+
+
+  let expand ~term ~ctx t1 t2 =
+    (* We first must determine which ambivalence we will
+       be expanding. *)
+    let t, (rigid_var, rigid_type) =
+      match get_min_expansive ~ctx t1, get_min_expansive ~ctx t2 with
+      | None, None -> raise Cannot_expand
+      | Some expansive, None -> t1, expansive
+      | None, Some expansive -> t2, expansive
+      | ( Some ((_, rigid_type1) as expansive1)
+        , Some ((_, rigid_type2) as expansive2) ) ->
+        if snd rigid_type1 < snd rigid_type2
+        then t1, expansive1
+        else t2, expansive2
+    in
+    (* We now add [rigid_type] to [t]. *)
+    (match fst rigid_type with
+    | Rigid_type.Rigid_var rigid_var' ->
+      (* Add [rigid_var'] to [rigid_vars] w/ the scope of the equation. *)
+      (match
+         Hashtbl.add t.rigid_vars ~key:rigid_var' ~data:(true, snd rigid_type)
+       with
+      | `Ok | `Duplicate -> ())
+    | Rigid_type.Structure structure ->
+      (match t.structure with
+      | Some _ -> ()
+      | None ->
+        (* Convert [structure] using [term]. *)
+        let structure = Structure.map structure ~f:(convert_rigid_type ~term) in
+        t.structure <- Some (structure, snd rigid_type)));
+    (* Set [rigid_var]  to be non-expansive *)
+    Hashtbl.set t.rigid_vars ~key:(fst rigid_var) ~data:(false, snd rigid_var)
+
+
+  let merge ~term ~ctx ~equate t1 t2 =
+    let rec loop () =
+      try decompose ~term ~ctx ~equate t1 t2 with
+      | Structure.Cannot_merge -> raise Cannot_merge
+      | Cannot_decompose ->
+        (try expand ~term ~ctx t1 t2 with
+        | Cannot_expand -> raise Cannot_merge);
+        loop ()
+    in
+    loop ()
+end
---
+++ src/constraints/solver/structure_intf.ml
@@ -0,0 +1,120 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*                                Dromedary                                  *)
+(*                                                                           *)
+(*                Alistair O'Brien, University of Cambridge                  *)
+(*                                                                           *)
+(* Copyright 2021 Alistair O'Brien.                                          *)
+(*                                                                           *)
+(* All rights reserved. This file is distributed under the terms of the MIT  *)
+(* license, as described in the file LICENSE.                                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(* This module implements signatured used for unification structures. *)
+
+module type S = sig
+  (** A structure defines the "structure" of the unification type.
+      Namely, the unification type is given by the fixed point of the 
+      functor ['a structure].
+      
+      Some examples of structures include:
+      - First order terms
+      - Rigid first terms
+      - Rows
+      - Ambivalence
+  *)
+  type 'a t
+
+  val map : 'a t -> f:('a -> 'b) -> 'b t
+  val iter : 'a t -> f:('a -> unit) -> unit
+  val fold : 'a t -> f:('a -> 'b -> 'b) -> init:'b -> 'b
+
+  (** Structures must exhibit [merge], which is the ability
+      to compute a logically consistent structure from 2 structures. 
+
+      If the 2 structures are not consistent, then we raise [Cannot_merge].
+      
+      Consistency is determined via the ability to emit first-order equalities,
+      provided by [equate].
+
+      In some cases, logical consistency of 2 structures requires a context. 
+      (e.g. Abbreviations, and Ambivalence), thus [merge] requires a
+      context [ctx]. 
+      
+      In some cases, consistency may also fold and unfold the fixed point, 
+      used by the unifier (e.g. Rows, Abbreviations and Ambivalence). This
+      is provided by the parameter [term].
+  *)
+
+  type term =
+    { fold : 'a. 'a t -> 'a
+    ; unfold : 'a. 'a -> 'a t
+    }
+
+  type ctx
+
+  exception Cannot_merge
+
+  val merge
+    :  term:term
+    -> ctx:ctx
+    -> equate:('a -> 'a -> unit)
+    -> 'a t
+    -> 'a t
+    -> 'a t
+end
+
+module type Intf = sig
+  module type S = S
+
+  module Rigid_var : sig
+    type t = private int [@@deriving compare]
+
+    val make : unit -> t
+    val hash : t -> int
+  end
+
+  module Amibvalence (S : S) : sig
+    module Rigid_type : sig
+      type t =
+        | Rigid_var of Rigid_var.t
+        | Structure of t S.t
+    end
+
+    module Equations : sig
+      module Scope : sig
+        (** [t] represents the "scope" of the equation. It is used to track 
+            consistency in level-based generalization *)
+        type t = int [@@deriving compare]
+
+        val outermost_scope : t
+      end
+
+      module Ctx : sig
+        (** [t] represents the equational scope used for Ambivalence *)
+        type t
+
+        (** [empty] is the empty equational context. *)
+        val empty : t
+
+        exception Inconsistent
+
+        (** [add t type1 type2 scope] adds the equation [type1 = type2] 
+            in the scope [scope]. *)
+        val add : t -> Rigid_type.t -> Rigid_type.t -> Scope.t -> t
+      end
+    end
+
+    type 'a t
+
+    val make_rigid_var : Rigid_var.t -> 'a t
+    val make_structure : 'a S.t option -> 'a t
+    val structure : 'a t -> 'a S.t option
+    val scope : 'a t -> Equations.Scope.t
+
+    type ctx = Equations.Ctx.t * S.ctx
+
+    include S with type 'a t := 'a t and type ctx := S.ctx
+  end
+end
