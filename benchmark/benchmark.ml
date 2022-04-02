57a7d4f6179d283e7b6f8fbd0843c7bc60edfc26
johnyob <ajo41@cam.ac.uk>
2022-04-02 18:47:53 +0100

    Record benchmarks, and analyze errors.

--- benchmark/benchmark.ml
+++ benchmark/benchmark.ml
@@ -1,9 +1,30 @@
 open Core
+open Core_bench
 
-let () =
+(* let () =
   Command_unix.run
     (Command.group
        ~summary:"Benchmarks"
        [ "infer", Benchmark_infer.command
        ; "stress-infer", Benchmark_infer.stress_command
-       ])
+       ]) *)
+
+let perform_benchmark tests =
+  let open Bench in
+  Bench.bench
+    ~run_config:
+      (Run_config.create
+         ~quota:(Quota.Num_calls 10000)
+         ~stabilize_gc_between_runs:true
+         ~fork_each_benchmark:true
+         ())
+    ~save_to_file:(fun measurement ->
+      "results/" ^ Measurement.name measurement ^ ".csv")
+    ~analysis_configs:
+      (Analysis_config.default
+      |> List.map
+           ~f:(Analysis_config.with_error_estimation ~bootstrap_trials:1000))
+    tests
+
+
+let () = perform_benchmark Benchmark_infer.stress_tests
